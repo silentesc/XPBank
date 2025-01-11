1ba3f536104d100d68bfad3aba63118b39d65d98
silentesc <116022641+silentesc@users.noreply.github.com>
2025-01-11 13:05:38 +0100

    Init

---
+++ src/main/java/de/silentesc/xpbank/Main.java
@@ -0,0 +1,12 @@
+package de.silentesc.xpbank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
